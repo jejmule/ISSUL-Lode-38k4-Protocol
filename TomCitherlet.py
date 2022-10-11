5f481c4a7451739d68321a670c674d6758193bd0
Jerome Parent <jejmule@gmail.com>
2022-10-11 15:08:30 +0200

    devices implemented

--- TomCitherlet.py
+++ TomCitherlet.py
@@ -5,7 +5,7 @@ Created on Tue Dec  8 16:50:30 2020
 @author: jparent1
 """
 
-import time
+import os,time
 from msvcrt import getch
 from statistics import mean
 from datetime import datetime
@@ -19,36 +19,62 @@ def clear_line(n=1):
     for i in range(n):
         print(LINE_UP, end=LINE_CLEAR)
 
-ergo = lode38k4()
+def protocol(ergo, device = 1) : 
 
-minutes = 0
-seconds = 30
-step_s = 1
-duration_s = minutes * 60 + seconds
+    minutes = 0
+    seconds = 30
+    step_s = 1
+    duration_s = minutes * 60 + seconds
 
-print("\nExperiment duration "+str(duration_s)+" s")
-print("press any key to start or esc to quit")
-test = getch() 
-while test != b'\x1b' :
-    load = []
-    rpm = []
-    print(print("\nStart of test : "+str(datetime.now())))
-    for i in range(1,duration_s+1,step_s) :
-        if ergo.connected : 
-            load.append(ergo.get_load())
-            rpm.append(ergo.get_rpm_decimal())
-        else :
-            load.append(0)
-            rpm.append(0)
-        clear_line()
-        prefix = "Test running : "+str(i)+"/"+str(duration_s)+" s"
-        result = "\t Load : "+str(load[i-1])+" W"+"\t RPM :" +str(rpm[i-1])+" tr/min" 
-        if i == duration_s :
-            prefix = "Test results : "
-            result = "\tMean load : "+str(mean(load))+" W"+"\t Mean RPM :" +str(mean(rpm))+" tr/min"
-        print(prefix+result)
-        time.sleep(step_s)
-    print("End of test : "+str(datetime.now())+"\n")
-    print("press any key to start or esc to quit")
-    test = getch()
+    os.system('')
+
+    print("\nExperiment duration "+str(duration_s)+" s")
+    print("press "+str(device)+" to start on device "+str(device))
+    test = b'' 
+    while test != b'\x1b' :
+        if test.decode() == str(device) :
+            load = []
+            rpm = []
+            print("Start of test on device "+str(device)+" : "+str(datetime.now()))
+            for i in range(1,duration_s+1,step_s) :
+                if ergo.connected : 
+                    load.append(ergo.get_load(device))
+                    rpm.append(ergo.get_rpm_decimal(device))
+                else :
+                    load.append(0)
+                    rpm.append(0)
+                #clear_line()
+                prefix = "Test on device "+str(device)+" : "+str(i)+"/"+str(duration_s)+" s"
+                result = "\t Load : "+str(load[i-1])+" W"+"\t RPM :" +str(rpm[i-1])+" tr/min" 
+                if i == duration_s :
+                    prefix = "Test results on device "+str(device)+" : "
+                    result = "\tMean load : "+str(mean(load))+" W"+"\t Mean RPM :" +str(mean(rpm))+" tr/min"
+                    end = " press "+str(device)+" to re-start or esc to quit"
+                    print(prefix+result+end)
+                time.sleep(step_s)
+            #print("End of test on device "+str(device)+": "+str(datetime.now())+"\n")
+            #print("press "+str(device)+" to start on device "+str(device)+" or esc to quit")
+        test = getch()
+    
+    print("Exit protocol on device "+str(device))
+
+if __name__ == '__main__' :
+    import sys
+    from threading import Thread
+    args = sys.argv
+    del args[0] #remove script path form arg list
+    threads = []
+    ergo = lode38k4()
+    if args :
+        for arg in args :
+            print("device : ",arg)
+            t = Thread(target=protocol,args=(ergo,arg,))
+            t.start()
+            threads.append(t)
+    else :
+        protocol(ergo)
+
+    for thread in threads :
+        pass
+        #thread.join()
 
--- lode38k4.py
+++ lode38k4.py
@@ -14,8 +14,6 @@ baudrate = 38400
 class lode38k4 :
     
     def __init__(self,com_port="",device=1) :
-        
-        self.device = device
 
         if com_port == "" :
             port_list = ports.comports()
@@ -24,61 +22,67 @@ class lode38k4 :
                 print(port.device+" : "+port.description)
                 #test connectioon and lode request status command
                 self.serial = serial.Serial(str(port.device),timeout=timeout,baudrate=baudrate)
-                if self.check_port() :
+                if self.check_port(device) :
                     break
         else :
             self.serial = serial.Serial(com_port,timeout=timeout,baudrate=baudrate)
-            self.check_port()
+            self.check_port(device)
 
     
-    def check_port(self) : 
+    def check_port(self,device) : 
         try :
-            answer = self.get_status()
-            print("-LODE device : "+str(answer))
+            answer = self.get_serial(device)
+            print("-LODE device "+str(device)+", serial : "+ answer)
             self.connected = True
         except ValueError :
-            print("-LODE device not found ")
+            print("-LODE device "+str(device)+" not found ")
             self.connected = False
         
-    def set_power(self,power) :
+    def set_power(self,device,power) :
         command = 'SP{0}'.format(power)
-        answer = self.request(command)
+        answer = self.request(device,command)
         
         return self.acq(answer)
     
-    def set_torque(self,torque) :
+    def set_torque(self,device,torque) :
         command = 'ST{0}'.format(int(torque*100))
-        answer = self.request(command)
+        answer = self.request(device,command)
         
         return self.acq(answer)
     
-    def get_status(self) :
+    def get_status(self,device) :
         command = 'RS'
-        answer = self.request(command)
+        answer = self.request(device,command)
         return int(answer)
+    
+    def get_serial(self,device) :
+        command = 'SN'
+        answer = self.request(device,command)
+        return answer
         
-    def get_load(self) :
+    def get_load(self,device) :
         command  = 'PM'
-        answer = self.request(command)
+        answer = self.request(device,command)
         return int(answer)
     
-    def get_rpm(self) :
+    def get_rpm(self,device) :
         command  = 'RM'
-        answer = self.request(command)
+        answer = self.request(device,command)
         return int(answer)
     
-    def get_rpm_decimal(self):
+    def get_rpm_decimal(self,device):
         command = 'RN'
-        answer = self.request(command)
+        answer = self.request(device,command)
         rpm = int(answer)/10
         return rpm
     
-    def request(self,command) :
-        query = '{0},'.format(self.device)+command+'\r'
+    def request(self,device,command) :
+        query = '{0},'.format(device)+command+'\r'
         self.serial.write(query.encode('ascii'))
         response =self.serial.readline()
-        [device,answer] = response.decode('ascii').strip('\r').split(',')
-        return answer
+        [device_id,answer] = response.decode('ascii').strip('\r').split(',')
+        if device_id == device :
+            return answer
     
     def acq(self,answer) : 
         if answer == '\x06' :
